100d35d06c8f7770b5c8185cb856e5d042bb23b9
Sanket Rajgarhia <sanket.rajgarhia@gmail.com>
2022-04-02 07:46:38 +0530

    Updated the Customer class to have the additionalInformation field.

--- js/model/customer.js
+++ js/model/customer.js
@@ -13,15 +13,17 @@
 class Customer {
 
     /* *Constructor
-     * @param    {String} customerName    Name of the customer
-     * @param    {String} phoneNumber    10 Digit Phone Number of the customer
-     * @param    {String} address        150 characters address
+     * @param    {String} customerName          Name of the customer
+     * @param    {String} phoneNumber           10 Digit Phone Number of the customer
+     * @param    {String} address               150 characters address
+     * @param    {String} additionalInformation 250 characters additional information
      * */
-    constructor(customerName, phoneNumber, address) {
+    constructor(customerName, phoneNumber, address, additionalInformation) {
 
         this._customerName = customerName;
         this._phoneNumber = phoneNumber;
         this._address = address;
+        this._additionalInformation = additionalInformation;
 
     }
 
@@ -65,6 +67,20 @@ class Customer {
      */
     set address(address) {
         this._address = address;
+    }
+
+    /* Get the additional information
+     * @return  {String} Additional information
+     */
+    get additionalInformation() {
+        return this._additionalInformation;
+    }
+
+    /* Set the additional information
+     * @param   {String} additionalInformation  250 characters additional information
+     */
+    set additionalInformation(additionalInformation) {
+        this._additionalInformation = additionalInformation;
     }
 
 }
